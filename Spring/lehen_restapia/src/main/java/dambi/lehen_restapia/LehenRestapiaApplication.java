21412c01121ce411ba965ded65a8b05ee4bdda6c
Markel Lomana <lomana.markel@uni.eus>
2022-11-21 11:56:46 +0100

    update folder

---
+++ Spring/lehen_restapia/src/main/java/dambi/lehen_restapia/LehenRestapiaApplication.java
@@ -0,0 +1,13 @@
+package dambi.lehen_restapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LehenRestapiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LehenRestapiaApplication.class, args);
+	}
+
+}
