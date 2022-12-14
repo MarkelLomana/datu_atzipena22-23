ec52e5ebd33858b1b2801fd893e4b523d0d1bbb6
Markel <lomana.markel@uni.eus>
2022-12-14 11:27:34 +0100

    client

--- Spring/accesing-data-mongodb/src/main/java/dambi/App.java
+++ Spring/accesing-data-mongodb/src/main/java/dambi/App.java
@@ -1,14 +1,23 @@
 package dambi;
 
+import com.mongodb.client.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        MongoClient.main(args);
+public class App {
+  public static void main(String[] args) {
+    String uri = "mongodb+srv://user1:user1@cluster0.wcdu5.mongodb.net/?retryWrites=true&w=majority";
+    try (MongoClient mongoClient = MongoClients.create(uri)) {
+      MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+      MongoCollection<Document> collection = database.getCollection("movies");
+      Document doc = collection.find(eq("title", "Back to the Future")).first();
+      if (doc != null) {
+        System.out.println(doc.toJson());
+      } else {
+        System.out.println("No matching documents found.");
+      }
     }
+  }
 }
